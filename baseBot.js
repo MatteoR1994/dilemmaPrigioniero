cee0fd9b34a23894ecc254792ac4e1bc30fb529b
MatteoR1994 <matteo.rinaldi1893@gmail.com>
2022-02-18 09:22:27 +0100

    Create baseBot.js

---
+++ baseBot.js
@@ -0,0 +1,19 @@
+class BaseBot {
+
+    constructor() {
+        
+    }
+
+    play() {
+        return 1;
+    }
+
+    newGame() {
+
+    }
+
+    memorize(number) { // number sarà 0 o 1 = l'ultimo comportamento dell'avversario
+
+    }
+
+}
